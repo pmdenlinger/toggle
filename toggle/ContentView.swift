4e22e71d3e4a263aac25be5dacf242700748f71d
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 17:24:04 -0700

    Initial Commit

---
+++ toggle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toggle
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toggle/toggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggleApp.swift
+//  toggle
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct toggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
